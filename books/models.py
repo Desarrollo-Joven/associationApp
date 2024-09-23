fb51c5ff16ba0cd8f34c61ef05c152bad73057c0
Desarrollo-Joven <desarrollojoven@ciditpy.org>
2024-09-23 10:31:21 -0400

    init

---
+++ books/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'books'
+
+    def __str__(self):
+        return f'{self.title}'
